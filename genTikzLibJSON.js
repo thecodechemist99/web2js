15da16adfefe4cea8c17a863a1bc35247b7fa90c
Glenn Rice <grice1@missouriwestern.edu>
2021-01-03 17:22:11 -0600

    Move the tikz library test files to tikz_libs, and move the other tex test files to tex_packages. Add a node script that runs tex on all of the tikz_lib files and generates the JSON file used by tikzjax.

---
+++ genTikzLibJSON.js
@@ -0,0 +1,35 @@
+var path = require('path');
+var fs = require('fs');
+var spawnSync = require('child_process').spawnSync;
+
+const basePath = '/usr/share/texlive/texmf-dist/tex/generic/pgf/frontendlayer/tikz/libraries';
+
+fs.mkdirSync('tikz_libs', { recursive: true });
+
+function processDir(dir) {
+	for (let file of fs.readdirSync(dir, { withFileTypes: true })) {
+		if (file.isDirectory()) {
+			processDir(path.resolve(basePath, file.name));
+		} else if (file.name.match(/^tikzlibrary.*\.code\.tex$/)) {
+			let tikzLibName = file.name.replace(/^tikzlibrary(.*)\.code\.tex$/, "$1");
+			console.log(`Processing ${tikzLibName}`);
+			let texFile = `tikz_libs/${tikzLibName}.tex`;
+			if (!fs.existsSync(texFile)) {
+				console.log(`Creating ${texFile}`);
+				fs.writeFileSync(texFile,
+					`%\\documentclass[margin=0pt]{standalone}
+%\\usepackage{tikz}
+\\usetikzlibrary{${tikzLibName}}
+\\begin{document}
+\\end{document}`
+				);
+			}
+			console.log(`Running TeX on ${texFile}`);
+			fs.writeFileSync(`tikz_libs/${tikzLibName}.output.log`,
+				spawnSync('node', ['tex.js', texFile]).stdout.toString());
+			console.log(`TeX output saved to tikz_libs/${tikzLibName}.output.log`);
+		}
+	}
+}
+
+processDir(basePath);
